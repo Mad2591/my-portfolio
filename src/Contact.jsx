84eb33b7f192696f29cab9fb0e096eac74c5b618
Madhuri Patil <madhuripatil2591@gmail.com>
2024-05-27 12:44:22 +0530

    new file:   src/assets/Madhuri.pdf

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Contact.css';
+import pdf from './assets/Madhuri.pdf'
 
 const Contact = () => {
   return (
@@ -7,8 +8,7 @@ const Contact = () => {
       <h2 className="contact-heading">Contact Me</h2>
       <p className="contact-info">Email: madhuripatil2591@gmail.com</p>
       <div className="cv-download">
-        <a href="Your_CV_Download_Link" className="cv-button">Download CV</a>
-        <p className="cv-note">*Please contact me directly for my CV.</p>
+        <a href={pdf}  download className="cv-button">Download CV</a>
       </div>
     </div>
   );
